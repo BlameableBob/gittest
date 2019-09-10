5cb7387b69e9400f516f3f0d6fd703bb52301490
BlameableBob <bob_broekmeulen@hotmail.com>
2019-09-10 10:38:51 +0200

    yep

--- src/main/java/com/novi/gittest/Main.java
+++ src/main/java/com/novi/gittest/Main.java
@@ -15,7 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("dit is BOB");
+        System.out.println("WAT");
     }
     
 }
